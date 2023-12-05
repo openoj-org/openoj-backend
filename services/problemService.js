67180569949fe357ae44f301f2f43535066c4308
zengxb20 <zengxb20@mails.tsinghua.edu.cn>
2023-12-05 21:27:43 +0800

    change problem

--- services/problemService.js
+++ services/problemService.js
@@ -12,6 +12,10 @@ const {
 // const { setCookie } = require('undici-types');
 // const { user } = require('../db/dbConfig');
 var multiparty = require('multiparty');
+const { select_user_id_by_cookie, select_user_by_id } = require('../CURDs/userCURD');
+const fs = require('fs');
+const compressing = require('compressing');
+const xlsx = require('xlsx');
 
 // 检查器函数, func 为 CURD 函数, isDefault 表示是否使用默认 JSON 解析
 function validateFunction(req, res, next, func, isDefault) {
@@ -64,40 +68,309 @@ function problem_delete(req, res, next) {
 // 用文件修改题目
 function problem_change_by_file(req, res, next) {
   validateFunction(req, res, next, (req, res, next) => {
-    // TODO
+    let form = new multiparty.Form({ uploadDir: './static' });
+    return form.parse(req, async (err, fields, files) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: err
+        });
+      } else {
+        let { cookie, id} = fields;
+        // select_user_id_by_cookie(cookie)
+        // .then(usrid => {
+        //   if(usrid.success) {
+        //     select_user_by_id(usrid.id)
+        //     .then(usr => {
+        //       if(usr.success) {
+        //         if(usr.character >1) {
+        //           res.json({
+        //             success: false,
+        //             message: '无管理权限'
+        //           });
+        //         } else {
+                  let file = files.data[0];
+                  let filename = "./static/官方题库/"+id;
+                  compressing.zip.uncompress(file.path, filename)
+                  .then(() => {
+                      console.log('success');
+                      fs.unlinkSync(file.path);
+                      //TODO 解析data文件
+                  })
+                  .catch(err => {
+                      console.error(err);
+                  });
+        //         }
+        //       } else {
+        //         res.json({
+        //           success: false,
+        //           message: usr.message
+        //         });
+        //       }
+        //     })
+        //     .catch(errorObj => {
+        //       res.json(errorObj);
+        //     });
+        //   } else {
+        //     res.json({
+        //       success: false,
+        //       message: usrid.message
+        //     });
+        //   }
+        // })
+        // .catch(errorObj => {
+        //   res.json(errorObj);
+        // });
+      }
+    });
   }, false);
 }
 
 // 修改题目数据
 function problem_change_data(req, res, next) {
   validateFunction(req, res, next, (req, res, next) => {
-    // TODO
+    let form = new multiparty.Form({ uploadDir: './static' });
+    return form.parse(req, async (err, fields, files) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: err
+        });
+      } else {
+        let { cookie, id} = fields;
+        // select_user_id_by_cookie(cookie)
+        // .then(usrid => {
+        //   if(usrid.success) {
+        //     select_user_by_id(usrid.id)
+        //     .then(usr => {
+        //       if(usr.success) {
+        //         if(usr.character >1) {
+        //           res.json({
+        //             success: false,
+        //             message: '无管理权限'
+        //           });
+        //         } else {
+                  let file = files.data[0];
+                  let filename = "./static/官方题库/"+id;
+                  compressing.zip.uncompress(file.path, filename)
+                  .then(() => {
+                      console.log('success');
+                      fs.unlinkSync(file.path);
+                      //TODO 解析data文件
+                  })
+                  .catch(err => {
+                      console.error(err);
+                  });
+        //         }
+        //       } else {
+        //         res.json({
+        //           success: false,
+        //           message: usr.message
+        //         });
+        //       }
+        //     })
+        //     .catch(errorObj => {
+        //       res.json(errorObj);
+        //     });
+        //   } else {
+        //     res.json({
+        //       success: false,
+        //       message: usrid.message
+        //     });
+        //   }
+        // })
+        // .catch(errorObj => {
+        //   res.json(errorObj);
+        // });
+      }
+    });
   }, false);
 }
 
 // 修改题目元数据
 function problem_change_meta(req, res, next) {
   validateFunction(req, res, next, (req, res, next) => {
-    // TODO
+    let { cookie, id, title, titleEn, type, timeLimit, memoryLimit, background, statement, inputStatement, outputStatement, rangeAndHint, source} = req.body;
+    // select_user_id_by_cookie(cookie)
+    // .then(usrid => {
+    //   if(usrid.success) {
+    //     select_user_by_id(usrid.id)
+    //     .then(usr => {
+    //       if(usr.success) {
+    //         if(usr.character >1) {
+    //           res.json({
+    //             success: false,
+    //             message: '无管理权限'
+    //           });
+    //         } else {
+    //            //TODO:向数据库中修改数据 update_problems()
+    //            .then(result => {})
+    //            .catch()
+    //         }
+    //       } else {
+    //         res.json({
+    //           success: false,
+    //           message: usr.message
+    //         });
+    //       }
+    //     })
+    //     .catch(errorObj => {
+    //       res.json(errorObj);
+    //     });
+    //   } else {
+    //     res.json({
+    //       success: false,
+    //       message: usrid.message
+    //     });
+    //   }
+    // })
+    // .catch(errorObj => {
+    //   res.json(errorObj);
+    // });
   }, false);
 }
 
 // 用文件创建题目
 function problem_create_by_file(req, res, next) {
   validateFunction(req, res, next, (req, res, next) => {
-    // TODO
+    let form = new multiparty.Form({ uploadDir: './static' });
+    return form.parse(req, async (err, fields, files) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: err
+        });
+      } else {
+        let { cookie, id} = fields;
+        // select_user_id_by_cookie(cookie)
+        // .then(usrid => {
+        //   if(usrid.success) {
+        //     select_user_by_id(usrid.id)
+        //     .then(usr => {
+        //       if(usr.success) {
+        //         if(usr.character >1) {
+        //           res.json({
+        //             success: false,
+        //             message: '无管理权限'
+        //           });
+        //         } else {
+                  //TODO 将以上信息存入数据库
+                  let file = files.data[0];
+                  let filename = "./static/官方题库/"+id;
+                  compressing.zip.uncompress(file.path, filename)
+                  .then(() => {
+                      console.log('success');
+                      fs.unlinkSync(file.path);
+                      
+                  })
+                  .catch(err => {
+                      console.error(err);
+                  });
+        //         }
+        //       } else {
+        //         res.json({
+        //           success: false,
+        //           message: usr.message
+        //         });
+        //       }
+        //     })
+        //     .catch(errorObj => {
+        //       res.json(errorObj);
+        //     });
+        //   } else {
+        //     res.json({
+        //       success: false,
+        //       message: usrid.message
+        //     });
+        //   }
+        // })
+        // .catch(errorObj => {
+        //   res.json(errorObj);
+        // });
+      }
+    });
   }, false);
 }
 
 // 创建题目
 function problem_create(req, res, next) {
   validateFunction(req, res, next, (req, res, next) => {
-    let form = new multiparty.Form();
-    form.parse(req, function(err,fields,file){
-      
-
+    let form = new multiparty.Form({ uploadDir: './static' });
+    return form.parse(req, async (err, fields, files) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: err
+        });
+      } else {
+        let { cookie, id, title, titleEn, type, timeLimit, memoryLimit, background, statement, inputStatement, outputStatement, rangeAndHint, source} = fields;
+        // select_user_id_by_cookie(cookie)
+        // .then(usrid => {
+        //   if(usrid.success) {
+        //     select_user_by_id(usrid.id)
+        //     .then(usr => {
+        //       if(usr.success) {
+        //         if(usr.character >1) {
+        //           res.json({
+        //             success: false,
+        //             message: '无管理权限'
+        //           });
+        //         } else {
+                  //TODO 将以上信息存入数据库
+                  let file = files.data[0];
+                  let filename = "./static/官方题库/"+id;
+                  compressing.zip.uncompress(file.path, filename)
+                  .then(() => {
+                      console.log('success');
+                      fs.unlinkSync(file.path);
+                      
+                  })
+                  .catch(err => {
+                      console.error(err);
+                  });
+        //         }
+        //       } else {
+        //         res.json({
+        //           success: false,
+        //           message: usr.message
+        //         });
+        //       }
+        //     })
+        //     .catch(errorObj => {
+        //       res.json(errorObj);
+        //     });
+        //   } else {
+        //     res.json({
+        //       success: false,
+        //       message: usrid.message
+        //     });
+        //   }
+        // })
+        // .catch(errorObj => {
+        //   res.json(errorObj);
+        // });
+      }
     });
   }, false);
+}
+
+function decodeXlsx(xlsxpath){
+  let ret = new Array();
+  let workbook = xlsx.readFile(xlsxpath);
+  let sheetNames = workbook.SheetNames;
+  let sheet1 = workbook.Sheets[sheetNames[0]];
+  let range = xlsx.utils.decode_range(sheet1['!ref']);
+  for (let R = range.s.r; R <= range.e.r; ++R) {
+      ret[R] = new Array();
+      for (let C = range.s.c; C <= range.e.c; ++C) {
+          let cell_address = {c: C, r: R};
+          let cell = xlsx.utils.encode_cell(cell_address);
+          if (sheet1[cell]) {
+          ret[R][C] = sheet1[cell].v;
+          }
+      }
+  }
+  return ret;
 }
 
 module.exports = {
