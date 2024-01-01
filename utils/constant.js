31477e59419acb1b674d6980154f20e82031e23e
Mr_Spade <Mr_Spade@foxmail.com>
2024-01-01 22:59:58 +0800

    update judger url

--- utils/constant.js
+++ utils/constant.js
@@ -4,5 +4,5 @@ module.exports = {
   CODE_TOKEN_EXPIRED: 200,
   PRIVATE_KEY: "zengxb20", // 自定义jwt加密的私钥
   JWT_EXPIRED: 60 * 60 * 24,
-  JUDGE_URL: "http://localhost:5000",
+  JUDGE_URL: "http://judger:5001",
 };
